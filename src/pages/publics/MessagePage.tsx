6c4cf5d9f7ea98b0c58515d9409e9d56d4be966b
Eko Saputra <Ekosaputra.t.i@gmail.com>
2025-01-22 20:32:42 +0700

    push

--- src/pages/publics/MessagePage.tsx
+++ src/pages/publics/MessagePage.tsx
@@ -1,26 +1,13 @@
 const MessagePage = () => {
     return (
         <div className='w-full h-screen flex flex-col items-center justify-center'>
-            <div className='w-6/12 m-auto bg-white shadow-md rounded-lg'>
+            <div className='w-full xl:w-6/12 md:w-6/12 lg:w-6/12 m-auto bg-white shadow-md rounded-lg'>
                 <div className='w-full p-4 bg-green-200 rounded-t-lg'>
                     <h1 className='font-bold text-2xl'>
-                        Selamat, Pendaftaran berhasil...
+                    Selamat pendaftaran yang kamu lakukan berhasil.
                     </h1>
-                    <h1 className='font-medium text-lg'>Kami akan mengirimkan invoice dan informasi lebih lanjut ke no whatsApp Kamu</h1>
+                    <h1 className='font-medium text-lg'>Tim kami akan mengirimkan invoice dan informasi lebih lanjut melalui WhatsApp. Terimakasih</h1>
                 </div>
-                <div className='p-4 bg-orange-100 rounded-b-lg'>
-                    <h1 className='font-medium'>Hati-hati penipuan! jangan menerima tagihan selain dari no berikut</h1>
-                    <h1 className='font-medium'>Admin 1 : +6285271726006</h1>
-                    <h1 className='font-medium'>Admin 2 : +6287739342149</h1>
-                    <h1 className='font-medium'>Admin 3 : +6289514268527</h1>
-                    <h1 className='font-medium'>Admin 4 : +6289675659580</h1>
-                    <h1 className='font-medium'>Admin 5 : +6289675659580</h1>
-                    <h1 className='font-medium'>Admin 6 : +6287821594060</h1>
-                    <h1 className='font-medium'>Telepon : (0274)2250131</h1>
-                    <h1 className='font-medium'>Kami tidak bertanggung jawab atas tagihan yang dikirimkan selain no diatas</h1>
-                    <h1 className='font-medium'>Terima Kasih</h1>
-                </div>
-                
             </div>
         </div>
     )
